aa9046a2476c0f42cb0f6293178d90fb87c43116
Robert Durst <me@robdurst.com>
2020-04-14 12:15:52 -0700

    make handler an object and add every condition

--- HydraFriend.js
+++ HydraFriend.js
@@ -1,4 +1,4 @@
-/*
+/**
  * @robertDurst 2020
  * 
  * a series of helper functions for quicker, cooler, more rad hydra visuals for
@@ -21,14 +21,84 @@ const parseTidal = (args) => {
     return obj
 };
 
-// generic function handler factory
-const handler = (bind_sound, do_somthing) => {
-    return (tidal_sound) => {
-        if (tidal_sound === bind_sound) {
-            do_somthing();
+/**
+ * All possible events in this JavaScript-y enum, which is one of the best ways
+ * to do enums according to:
+ * https://www.sohamkamani.com/blog/2017/08/21/enums-in-javascript/
+ * 
+ * Currently supported conditions:
+ *      * every: execute every x times
+ */
+const CONDITION_TYPES = {
+    EVERY: "every",
+}
+
+class Handler {
+    constructor(bind_sample, do_somthing) {
+        this.bind_sample = bind_sample;
+        // conditions that can be placed on when method should be executed
+        this.conditions = {}
+        // method invoked whenever sample played and all conditions met
+        this.do_somthing = do_somthing;
+        this._update_handler();
+    }
+
+    // internal method for checking against various conditions for the handler
+    // to execute (i.e. run every two times)
+    _check_conditions() {
+        const condition_keys = Object.keys(this.conditions);
+        for (let i = 0; i < condition_keys.length; i++) {
+            const condition = this.conditions[condition_keys[i]];
+            if (!condition()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // internal method for updating handler when properties change
+    _update_handler() {
+        this.handler = (tidal_sound) => {
+            if (tidal_sound === this.bind_sample && this._check_conditions()) {
+                this.do_somthing();
+            }
         }
     }
-};
+
+    // public interface for updating sample to match against
+    sample(bind_sample) {
+        this.bind_sample = bind_sample;
+        this._update_handler();
+    }
+
+    // update method called whenever sample property met
+    method(do_somthing) {
+        this.do_somthing = do_somthing;
+        this._update_handler();
+    }
+
+    // only play every x times
+    every(x) {
+        const iterations = x;
+        let iterations_left = x;
+        const condition = () => {
+            iterations_left--;
+            if (iterations_left == 0) {
+                iterations_left = iterations;
+                return true;
+            }
+            return false;
+        }
+
+        this.conditions[CONDITION_TYPES.EVERY] = condition;
+    }
+
+    // method called externally to execute underlying method
+    _exec(tidal_sound) {
+        this.handler(tidal_sound)
+    }
+}
 
 // HydraFriend is a registry for handlers, keeping track of relevant state,
 // visualizers (like osc) and listening for emitted events from SuperCollider
@@ -39,19 +109,20 @@ class HydraFriend {
             const tidal = parseTidal(args);
             setTimeout(() => {
                 this.handlers.forEach(handle => {
-                    handle(tidal.s);
+                    handle._exec(tidal.s);
                 });
             }, (tidal.delay * 1000))
         });
     }
 
     // TODO: deal with unique handles
-    register(bind_sound, do_somthing) {
-        this.handlers.push(handler(bind_sound, do_somthing));
+    register(handler) {
+        this.handlers.push(handler);
     }
 }
 
 module.exports = {
     initialize,
     HydraFriend,
+    Handler,
 }
